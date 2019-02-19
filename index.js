bcb562deaf4e36d71f0acd96cf3f4dd36fd6621c
Bara Limbong <baralimbong@gmail.com>
2019-02-19 16:37:25 +0700

    submit new id

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require ('body-parser')
 const port = 8000
-
-const users = [
+let NextId = 5
+app.use(bodyParser.json())
+Let users = [
   {
     id: 1,
     name: 'bara'
@@ -32,5 +34,20 @@ app.get('/users', (req, res) => {
     message: 'list of users',
     data: users
   })
+})
+app.post('/users', (req, res) => {
+    if(req.body.name){
+        const newUser= {
+            id:NextId,
+            name: req.body.name
+        }}
+        users: users.concat(newUser)
+        NextId++
+        res.send({
+            message: 'created new user',
+            newUser: newUser,
+            Data: users
+        })
+        
 })
 app.listen(port, () => console.log(`example app listening on port ${port}`))
