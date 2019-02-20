8d35ccf8cc06b1de90d754405ddd67d2f1840a44
Bara Limbong <baralimbong@gmail.com>
2019-02-20 11:54:51 +0700

    implement delete ad put API

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const bodyParser = require ('body-parser')
+const bodyParser = require('body-parser')
 const port = 8000
-let NextId = 5
+let nextId = 5
 app.use(bodyParser.json())
-Let users = [
+let users = [
   {
     id: 1,
     name: 'bara'
@@ -35,19 +35,55 @@ app.get('/users', (req, res) => {
     data: users
   })
 })
+
 app.post('/users', (req, res) => {
-    if(req.body.name){
-        const newUser= {
-            id:NextId,
-            name: req.body.name
-        }}
-        users: users.concat(newUser)
-        NextId++
-        res.send({
-            message: 'created new user',
-            newUser: newUser,
-            Data: users
-        })
-        
+  const newUser = {
+    id: nextId,
+    name: req.body.name
+  }
+
+  const newUsers = users.concat(newUser)
+  nextId++
+  users = newUsers
+  res.send({
+    message: 'created new user',
+    newUser: newUser,
+    users: users
+  })
 })
+
+app.delete('/users', (req, res) => {
+  users = []
+  res.send({
+    message: 'All User has been deleted',
+    users: users
+  })
+})
+
+app.delete('/users/id:', (req, res) => {
+  const id = number(req.params.id)
+  users = users.filter(user => user.id !== id)
+  res.send({ message: 'one user has been removed', id: id })
+})
+
+app.put('/users/id:', (req, res) => {
+  const id = number(req.params.id)
+  const newName = req.body.name
+
+  const newUsers = users.map(user => {
+    if (user.id === id) {
+      user.name = newName
+      return user
+    } else {
+      return user
+    }
+  })
+
+  res.send({
+    message: 'one user has been updated',
+    id: id,
+    newName: newName
+  })
+})
+
 app.listen(port, () => console.log(`example app listening on port ${port}`))
